ce2cf48b05b2d72fa14c5fecdac8e4f1a1fdaa13
deveshjain2511 <86546020+pragyamahajan@users.noreply.github.com>
2023-07-15 01:52:21 +0530

    Update check.js

--- check.js
+++ check.js
@@ -8,7 +8,7 @@ module.exports=(req,res,next)=>{
         next()
     }catch(e){
         return res.status(401).json({
-            msg:"invalid tpken"
+            msg:"invalid t0ken"
         })
     }
 }
